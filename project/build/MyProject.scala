9db74b8008e71a05c1b0af73d6023f41e524170b
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2011-05-06 16:37:44 +0900

    update scalatest

--- project/build/MyProject.scala
+++ project/build/MyProject.scala
@@ -1,5 +1,5 @@
 import sbt._
 
 class MyProject(info: ProjectInfo) extends DefaultProject(info){
-  val scalaTest = "org.scalatest" % "scalatest" % "1.2"
+  val scalaTest = "org.scalatest" % "scalatest" % "1.3"
 }
